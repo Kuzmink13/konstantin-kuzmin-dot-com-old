1cfb4bcaa9ace84879fef02404d2cf578d92a752
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-02-10 05:57:33 -0600

    misc: about selection corrections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="sm:leading-relaxed max-w-lg mx-auto p-6 mb-8">
         <p className="pb-3">
           <strong>Hey, I’m Konstantin.</strong> I got started in software
-          development by auditing free MIT courses while quarantining during the
+          development by auditing free MIT courses while quarantining from the
           covid-19 pandemic. Since then, I’ve finished my first major web
           project and am now looking to get into the industry so I can continue
           to build my skill set and meet my personal and professional goals.
@@ -32,7 +32,7 @@ export default function Home() {
         <p className="">
           My career interests include music education and general productivity
           tools. I believe there is massive potential in using technology to
-          help make people into the best versions of themselves. We are just
+          help make people into the best version of themselves. We are just
           scratching the surface of what tech can do in the fields of
           interpersonal business, communication, and mental health.
         </p>
