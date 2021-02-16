af906a0a11125cd9b8fb7d59246cb3a56b735707
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-02-16 11:18:42 -0600

    Removed index.html

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@
  */
 
 import React from 'react';
+import Head from 'next/head';
 
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
@@ -15,6 +16,20 @@ export default function MyApp({ Component, pageProps }) {
   const [isSidebarOpen, toggleSidebar] = useToggle();
   return (
     <div className="h-full w-full overflow-y-scroll flex flex-col justify-between">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="./favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Software Developer, Composer, Minnesotan"
+        />
+        <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
+        <link rel="manifest" href="./manifest.json" />
+        <title>Konstantin Kuzmin</title>
+      </Head>
+
       <Header {...{ isSidebarOpen, toggleSidebar }} />
       {isSidebarOpen && <Sidebar {...{ toggleSidebar }} />}
       <div className="flex flex-col flex-none">
