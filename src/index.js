b73df3cc5fbaadd0d5a64bf84a8082c08ceed9c0
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-02-16 08:54:48 -0600

    Removed tailwindcss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './index.css';
+// import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
