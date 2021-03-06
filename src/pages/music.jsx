3e715db2cac235f67b79fdd5d1be24d052a35aa8
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-03-06 07:58:42 -0600

    added blank pages

---
+++ src/pages/music.jsx
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) Konstantin Kuzmin. All Rights Reserved.
+ */
+
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+export default function music() {
+  return <Layout>music</Layout>;
+}
---
+++ src/pages/reviews.jsx
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) Konstantin Kuzmin. All Rights Reserved.
+ */
+
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+export default function reviews() {
+  return <Layout>reviews</Layout>;
+}
