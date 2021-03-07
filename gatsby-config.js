a564f899d706e11e54b9892f51c14660b0670a45
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-03-07 06:53:14 -0600

    Added contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
