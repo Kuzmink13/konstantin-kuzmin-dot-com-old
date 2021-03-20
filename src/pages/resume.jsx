81efd87333daf7bd1e813ab9b835be5537d6200f
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-03-20 13:51:10 -0500

    Created resume page

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -3,9 +3,61 @@
  */
 
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from '../components/Layout';
+import PageHeader from '../components/PageHeader';
+
+const query = graphql`
+  query {
+    contentfulPageContent(slug: { eq: "resume" }) {
+      pageTitle
+      pageDescription {
+        pageDescription
+      }
+    }
+  }
+`;
 
 export default function Resume() {
-  return <Layout>Resume</Layout>;
+  const { contentfulPageContent } = useStaticQuery(query);
+  return (
+    <Layout>
+      <div className="max-w-screen-xl mx-auto">
+        <PageHeader
+          title={contentfulPageContent.pageTitle}
+          description={contentfulPageContent.pageDescription.pageDescription}
+        />
+        <div className="flex flex-col max-w-sm mx-auto">
+          <StaticImage
+            className="shadow-xl border mt-2 md:mt-3 lg:mt-4
+            transform hover:scale-105
+            transition duration-700"
+            src="../assets/res.JPG"
+            width={384}
+            alt="resume screenshot"
+            placeholder="none"
+            formats={['auto', 'webp']}
+          />
+          <Download />
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+function Download() {
+  return (
+    <div className="flex justify-center my-10 md:my-12 lg:my-16">
+      <button type="button" className="btn">
+        <p
+          className="font-kanit text-base md:text-lg lg:text-lg
+          uppercase text-gray-100 font-medium tracking-widest"
+        >
+          download pdf
+        </p>
+      </button>
+    </div>
+  );
 }
