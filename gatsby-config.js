67490b91d78a495847d04ea11cbc69703b99972a
KuzminK13 <Konstantinkuzmin9@gmail.com>
2021-03-23 09:19:04 -0500

    Added Manifest Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,24 @@ module.exports = {
   siteMetadata: {
     title: 'Konstantin Kuzmin',
     author: 'Konstantin Kuzmin',
+    description:
+      'The personal website / portfolio of Konstantin Kuzmin, a software engineer from Minneapolis, Minnesota',
     copyright: '2021',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Konstantin Kuzmin`,
+        short_name: `Konstantin Kuzmin`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#f3f4f6`,
+        display: `standalone`,
+        icon: 'src/assets/site-icon-512x512.png',
+      },
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
